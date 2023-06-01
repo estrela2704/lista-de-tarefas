b7c5e9e8c275b20ced298a9fb6999121ec00b240
estrela2704 <felipeestrelatv@gmail.com>
2023-06-01 10:15:55 -0300

    Atualização final

--- model/tarefaModel.php
+++ model/tarefaModel.php
@@ -3,7 +3,14 @@ require_once('db.php');
 
 class Tarefa{
 
-
+    private $desc;
+    private $id;
+  
+    public function __construct($titulo, $desc, $id){
+        $this->titulo = $titulo;
+        $this->desc = $desc;
+        $this->id = $id;
+    }
 
 }
 
